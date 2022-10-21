a177c990b97a4e5fa961a420571f087bb39f15de
Ifyanyit <ngeneifyanyi@gmail.com>
2022-10-21 06:23:46 -0700

    Updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,5 +2,6 @@
 # Bash scripts that sends a POST request to a given URL.
 # and displays the body of the response
 # A variable email must be sent with test@gmail.com
-# A variable subject must be sent: I will always be here for PLD
+
+
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
